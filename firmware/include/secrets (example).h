7684b986beb00dcda3ac956d973a7b4f83c350f8
Javier Garcia <garcia.javier.alejandro@gmail.com>
2026-03-21 16:18:02 -0300

    fix: update WiFi credentials and MQTT user details in secrets file; correct NimBLE-Arduino dependency format in platformio.ini

--- firmware/include/secrets (example).h
+++ firmware/include/secrets (example).h
@@ -2,10 +2,10 @@
 
 #define WIFI_SSID "1234"
 #define WIFI_PASS "0123456789"
-#define WIFI_SSID_2 "Fibertel "
-#define WIFI_PASS_2 "1234"
+#define WIFI_SSID_2 "Fibertel WiFi324 2.4GHz"
+#define WIFI_PASS_2 "1148711236"
 #define WIFI_SSID_3 "Vhs2"
 #define WIFI_PASS_3 "VHS001248"
 
-#define MQTT_USER "ESP32-01"
-#define MQTT_PASS "1234"
+#define MQTT_USER "User-ESP32-01"
+#define MQTT_PASS "PASS-ESP32-01"
